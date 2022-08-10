7e33f0b9d0b6075c1f1d42840ef2c349d7ad2db3
Jr-Francis <fransales94jr@gmail.com>
2022-08-10 16:05:58 -0300

    att hippo

--- WSconsoleAPPHippo/Program.cs
+++ WSconsoleAPPHippo/Program.cs
@@ -8,11 +8,14 @@ internal class WSconsoleAPPHippo
     {
         string linkTxt = @"C:\Users\suele\Desktop\Maria Maria\VStudio22\HippoLinks.txt";
         string priceTxt = @"C:\Users\suele\Desktop\Maria Maria\VStudio22\Hippoprecos.txt";
+        //string linkTxt = @"C:\Users\Crawler\Desktop\Maria Maria\VStudio22\HippoLinks.txt";
+        //string priceTxt = @"C:\Users\Crawler\Desktop\Maria Maria\VStudio22\Hippoprecos.txt";
         var listaLinks = new List<string> { };
         var listaPrices = new List<string> { };
         string dadosListaLink;
 
         if (File.Exists(@"C:\Users\suele\Desktop\Maria Maria\VStudio22\HippoLinks.txt"))
+        //if (File.Exists(@"C:\Users\Crawler\Desktop\Maria Maria\VStudio22\HippoLinks.txt"))
         {
             using (StreamReader sr = new StreamReader(linkTxt)) //se arquivo n existir voltar p o menu
             {
@@ -24,23 +27,44 @@ internal class WSconsoleAPPHippo
                 }
                 sr.Close();
             }
+            var totalItens = listaLinks.Count;
+            Console.WriteLine("");
+            Console.WriteLine("Total de itens:");
+            Console.WriteLine(totalItens);
+            Console.WriteLine("");
 
-            IWebDriver driver = new ChromeDriver();
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArguments("headless");
+
+            IWebDriver driver = new ChromeDriver(chromeOptions);
+            var counter = 0;
 
             foreach (var product in listaLinks)
             {
-                Thread.Sleep(1000);
+                //Thread.Sleep(500);
                 driver.Navigate().GoToUrl(product);
 
-                Thread.Sleep(6000);
+                Thread.Sleep(2000);
                 var productOn = driver.FindElements(By.XPath("/html/body/app-root/app-produto-detalhe/div/div/div[1]/div/div/app-tag-preco/div/div[2]"));
-
-                if (productOn.Count > 0)
+                var productPromo2 = driver.FindElements(By.XPath("/html/body/app-root/app-produto-detalhe/div/div/div[1]/div/div/app-tag-preco/div[2]/div[2]"));
+                if(productPromo2.Count > 0)
+                {
+                    var pricePromo2 = driver.FindElement(By.XPath("/html/body/app-root/app-produto-detalhe/div/div/div[1]/div/div/app-tag-preco/div[2]/div[2]"));
+                    //var i = pricePromo.Count;
+                    listaPrices.Add(pricePromo2.Text);
+                    counter++;
+                    Console.WriteLine($"{counter} de {totalItens}");
+                    Console.WriteLine(pricePromo2.Text);
+                    Debug.Print(pricePromo2.Text);
+                }
+                else if (productOn.Count > 0)
                 {
                     // var pricePromo = driver.FindElement(By.XPath("/html/head/meta[13]"));
                     var pricePromo = driver.FindElement(By.XPath("/html/body/app-root/app-produto-detalhe/div/div/div[1]/div/div/app-tag-preco/div/div[2]"));
                     //var i = pricePromo.Count;
                     listaPrices.Add(pricePromo.Text);
+                    counter++;
+                    Console.WriteLine($"{counter} de {totalItens}");
                     Console.WriteLine(pricePromo.Text);
                     Debug.Print(pricePromo.Text);
                 }
@@ -48,6 +72,8 @@ internal class WSconsoleAPPHippo
                 {
                     Debug.Print("R$ 0,00");
                     listaPrices.Add("R$ 0,00");
+                    counter++;
+                    Console.WriteLine($"{counter} de {totalItens}");
                     Console.WriteLine("R$ 0,00");
                 }
                 
